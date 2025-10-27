e983399e68f5695e8128cb4bce7d658bf24640e3
auntim <hossennull@gmail.com>
2025-10-28 00:49:40 +0600

    full frontend and backend folder setup

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+const PORT = process.env.PORT || 5000;
+import { clerkMiddleware } from '@clerk/express'
+import { server } from '@inngest/express';
+import { inngest, functions } from './inngest/index.js';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(clerkMiddleware())
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+app.use("/api/inngest", server({ client: inngest, functions }));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
